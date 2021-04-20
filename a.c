ceb009307d632197273d82d4ac832371628ee065
joshuamv7 <joshua.moreira7@gmail.com>
2021-04-19 20:08:56 -0600

    Semaforos?

--- a.c
+++ a.c
@@ -9,6 +9,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define SEMAPHORES 1
+
 // New type definitions
 typedef struct {
     long timestamp;
@@ -21,18 +23,28 @@ PC_DATA *shared;
 int main(int argc, char* argv[]){
     
     int mem_desc; // shared memory descriptor
-
+    int sem_desc;
+    sem_t *sem;
     // Create shared mem in /dev/shm
     mem_desc = shm_open("/holis",
                   O_CREAT | O_RDWR,   // Create for R/W operations
                   S_IRUSR | S_IWUSR); // Give me R/W permission
     ftruncate(mem_desc, sizeof(PC_DATA));   // Set the shared memory size
+    sem = sem_open ("/Semaf", O_CREAT | O_EXCL, 0644, 1);
+    
 
     // Map (or "attach") the shared memory to an address in my process
     shared = mmap(NULL, sizeof(PC_DATA), 
             PROT_READ | PROT_WRITE,     // Enable Read/Write access
             MAP_SHARED,                 // Share among processes
             mem_desc, 0);
+   
+
+
+    sem_wait(sem);
+    shared->buff[0] =10;
     printf("hola a %d\n", shared->buff[0]);
-    printf("hola a %d\n", shared->buff[1]);
+    sleep(10);
+    sem_post(sem);
+    sem_unlink ("Semaf"); 
 }
--- b.c
+++ b.c
@@ -9,6 +9,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define SEMAPHORES 1
 // New type definitions
 typedef struct {
     long timestamp;
@@ -21,21 +22,28 @@ PC_DATA *shared;
 int main(int argc, char* argv[]){
     
     int mem_desc; // shared memory descriptor
+    int sem_desc;
+    sem_t *sem;
 
     // Create shared mem in /dev/shm
     mem_desc = shm_open("/holis",
                   O_CREAT | O_RDWR,   // Create for R/W operations
                   S_IRUSR | S_IWUSR); // Give me R/W permission
     ftruncate(mem_desc, sizeof(PC_DATA));   // Set the shared memory size
-
+    sem = sem_open ("/Semaf", O_CREAT | O_EXCL, 0644, 1);
+    
     // Map (or "attach") the shared memory to an address in my process
     shared = mmap(NULL, sizeof(PC_DATA), 
             PROT_READ | PROT_WRITE,     // Enable Read/Write access
             MAP_SHARED,                 // Share among processes
             mem_desc, 0);   
-    shared->buff[0] = 50;
-    shared->buff[1] = 505;
 
-    printf("hola %d\n",shared->buff[0]);
-    printf("hola %d\n",shared->buff[1]);
+    sem_wait(sem);
+    shared->buff[0] =5;
+    printf("hola a %d\n", shared->buff[0]);
+    sleep(10);
+    sem_post(sem);
+    sem_unlink ("Semaf"); 
+
+    // sem_post(semaforo);
 }
